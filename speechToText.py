a676d3e66e92a4a7021a0b59b2b956748b12f0bb
Danil Korotenko <danil.korotenko@gmail.com>
2020-03-11 16:41:37 +0200

    refactoring

--- speechToText.py
+++ speechToText.py
@@ -1,57 +1,100 @@
 #!/usr/bin/python
+
 import os, getopt, sys, json
+import ntpath
+import subprocess
+import codecs
+import base64
+
+#return string
+def encode_audio(audioFilePath):
+	audioFile = open(audioFilePath, "r")
+	audio_content = audioFile.read()
+	audioFile.close();
+	return base64.b64encode(audio_content)
+
+def recogniseFile(audioFilePath, api_key):
+	print "Processing file: ", audioFilePath;
+# 	print "Convert to flac";
+	flacPath = audioFilePath + '.flac';
+# 	print "flacPath: ", flacPath;
+	cmd = 'afconvert -f flac  -d flac "' + audioFilePath + '" "' + flacPath + '"';
+	os.system(cmd);
+	print "Do recognition";
+
+	audioContent = encode_audio(flacPath);
+	request = {
+		"config": {
+			"encoding":"FLAC",
+			"languageCode": "ru",
+		},
+		"audio": {
+			"content": audioContent
+		}
+	};
+
+	jsonFile = open("request.json", "w");
+
+	json.dump(request, jsonFile);
+
+	jsonFile.close();
+
+	recognitionCommand = 'curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json "https://speech.googleapis.com/v1/speech:recognize?key='+ api_key +'"';
+# 	print "recognitionCommand: ", recognitionCommand;
+
+	responseJson = os.popen(recognitionCommand).read();
+
+# 	print responseJson;
+	response = json.loads(responseJson);
+
+	print response['results'][0]['alternatives'][0]['transcript'];
+	print "********";
 
 def main(argv):
-    updateStorage = False
-    doRecognition = False
-    try:
-        opts, args = getopt.getopt(argv,"hur",["update","recognize"])
-    except getopt.GetoptError:
-        print 'speechToText.py -u -r'
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print 'speechToText.py -u -r'
-            sys.exit()
-        elif opt in ("-u", "--update"):
-            updateStorage = True
-        elif opt in ("-r", "--recognize"):
-            doRecognition = True
+	updateStorage = False;
+	doRecognition = False;
+	fileProcess = '';
+	api_key = '';
+	try:
+		opts, args = getopt.getopt(argv,"hrf:a:",["recognize","file","api-key"]);
+	except getopt.GetoptError:
+		sys.exit(2);
 
-    if updateStorage:
-        print "Updating storage..."
-        os.system('for i in ~/Documents/ViberDownloads/PTT/*.m4a; do baseFilename=`basename "${i}" .m4a` && afconvert -f flac  -d flac "${i}" "${baseFilename}.flac"; done')
 
-        os.system('gsutil cp ~/*.flac gs://optimum-habitat-169407/')
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'speechToText.py -u -r -f <filepath>';
+			sys.exit();
 
-#         os.system('rm ~/Documents/ViberDownloads/PTT/*.m4a')
+		if opt in ("-r", "--recognize"):
+			print "doRecognition is specified";
+			doRecognition = True;
 
-#         os.system('*.flac')
+		if opt in ("-f", "--file"):
+# 			print "File is specified: ", arg;
+			doRecognition = False;
+			updateStorage = False;
+			fileProcess = arg;
 
-        sys.exit()
+		if opt in ("-a", "--api-key"):
+# 			print "API key is specified: ", arg;
+			api_key = arg;
 
-    if doRecognition:
-        print "Do recognition"
-        ##os.system('gsutil ls -r gs://optimum-habitat-169407/**')
+	if api_key == '':
+		print "No API key specified."
+		sys.exit();
 
-#        responseJson = "{ \"results\": [ { \"alternatives\": [ { \"confidence\": 0.9527583, \"transcript\": \"\u041d\u0443 \u044f \u0445\u043e\u0447\u0443 \u0442\u0435\u0431\u0435 \u0441\u043a\u0430\u0437\u0430\u0442\u044c \u0447\u0442\u043e\" } ] } ] }"
+	if doRecognition:
+		print "Do recognition";
 
-#        response = json.loads(responseJson)
+		directory = "/Users/danilkorotenko/Documents/ViberDownloads/PTT/";
+		for filename in os.listdir(directory):
+			if filename.endswith(".m4a"):
+				fullPath = os.path.join(directory, filename);
+				recogniseFile(fullPath, api_key);
 
-        listOfFiles = os.popen('gsutil ls -r gs://optimum-habitat-169407/**').read().split('\n')
-
-        for c in listOfFiles:
-            if not c:
-                continue
-            recognitionCommand = "gcloud ml speech recognize %s --language-code='ru'" % (c)
-            responseJson = os.popen(recognitionCommand).read()
-            response = json.loads(responseJson)
-            print "File ", (c)
-            print response['results'][0]['alternatives'][0]['transcript']
-            print "********"
-#            break
+	if fileProcess:
+		recogniseFile(fileProcess, api_key);
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
-
-
+	main(sys.argv[1:]);
